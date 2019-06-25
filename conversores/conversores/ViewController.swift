6b44f3fc90cf3638c93518514e43736e5e187642
MacBookFcamara <gcmms@MacBook-Pro-de-Gabriel.local>
2019-06-25 16:14:52 -0300

    criação da tela

--- conversores/conversores/ViewController.swift
+++ conversores/conversores/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tfValor: UITextField!
+    
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResultado: UILabel!
+    @IBOutlet weak var lbResultadoUnit: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showMensagem(_ sender: UIButton) {
+    }
+    
+    @IBAction func converter(_ sender: Any) {
+    }
 }
 
