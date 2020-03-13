bfbac45d92593b553ca6d3fadd703aeff647b3d5
Gabriel Sanzone <gabriel.sanzone@fcamara.com.br>
2020-03-13 17:11:52 -0300

    Separando String da class principal

---
+++ conversores/Model/Grandeza/Temperatura/Temperatura.swift
@@ -0,0 +1,9 @@
+//
+//  Temperatura.swift
+//  conversores
+//
+//  Created by Gabriel Sanzone on 13/03/20.
+//  Copyright © 2020 Gabriel Chirico Mahtuk de Melo Sanzone. All rights reserved.
+//
+
+import Foundation
