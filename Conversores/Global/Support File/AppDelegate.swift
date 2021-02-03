e08f70ad020de671c77cb5f65d8d3dbad4bb3a51
Gabriel Sanzone <32177705+gcmms@users.noreply.github.com>
2021-02-03 16:20:14 -0300

    init cleancode arquitetura

--- Conversores/Global/Support File/AppDelegate.swift
+++ Conversores/Global/Support File/AppDelegate.swift
@@ -17,7 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GADMobileAds.sharedInstance().start(completionHandler: nil)
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+                var mainViewController = HomeViewController()
+
+                window?.rootViewController = mainViewController
+                window?.makeKeyAndVisible()
         return true
     }
 
---
+++ Conversores/Modulos/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  conversores
+//
+//  Created by Gabriel Chirico Mahtuk de Melo Sanzone on 03/02/21.
+//  Copyright © 2021 Gabriel Chirico Mahtuk de Melo Sanzone. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
